5404242f71d22e3eee3104e3eebc38113a342ae1
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-03 15:05:06 +0900

    redux

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,57 +1,62 @@
-import {createStore} from "redux";
+import{createStore} from "redux"    //redix에서 create store를 import
+const plus=document.getElementById("add")
+const minus=document.getElementById("minus")
+const number=document.getElementById("number")
 
 
+//reducer는 function 다.
+//data를 수정하기 위한 함수
+// reducer 은 data를 바꾸고 modify 하는걸 책임지는 
+//reducer 나 modifier는 처음으로 data를 바꿔준다.
 
-const add=document.getElementById('add');
-const minus=document.getElementById('minus');
-const number=document.querySelector('span');
-
-
-//string 을 keep 해주는 const
-
-const ADD="ADD";
-const MINUS="MINUS";
-
-
-
-const CountModifier=(count=0,action)=>{
-    //리덕스 에선 항상 상태(state)에 관해 애기할 때는 switch action.type이라고 작성
-    switch(action.type){
-        //만약 action type이 ADD 가 되면
-        case ADD:
-            return count +1
-            case MINUS:
-                return count -1
-                default:
-                    return count;
-    }
-   
-};
-
-
-
-
-const Countstore=createStore(CountModifier);
-const onChange=()=>{
-    number.innerText=Countstore.getState();
-}
-Countstore.subscribe(onChange);
-
-const handleAdd=()=>{
-   Countstore.dispatch({type:ADD});
-
-}
-const handleMinus=()=>{
-
-    Countstore.dispatch({type:MINUS});
-
+const countModifler =()=>{
+    return "hello";  //이러면 이게 내 data가 될 수 있다.
+ 
 }
 
-add.addEventListener("click",handleAdd);
-minus.addEventListener("click",handleMinus);
+
+
+//create 라는 함수가 우리에게 reducer 를 주기를 요구한다.
+const countStore=createStore(countModifler); //store 생성
+
+console.log(countStore.getState())
 
 
 
 
 
 
+
+
+
+//store는 data(state)를 넣는 공간이다
+//count를 modify(수정) 하기 위함 이다.
+//리덕스에는 createStroe 이라는 function 함수가 잇다
+//store가 하는 일은 기본적으로 너의 data를 넣을 수 있는 장소를 생성한다.
+//리덕스는 data를(관리하는데) 도와주는 역할을 하기 위해 만들어 졋다./4
+//이 케이스 에서는 redux 가 -1 +1 을  count 하는걸 도와주기 위해 만들어 졋다.
+//reducer 이 function 을 받아야 한다.
+
+
+
+
+
+// let count = 0;
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count = count + 1;
+//   updateText();
+// };
+
+// const handleMinus = () => {
+//   count = count - 1;
+//   updateText();
+// };
+
+// plus.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
