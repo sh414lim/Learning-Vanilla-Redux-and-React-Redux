f44bfc46be5eb318027ee11282dac14bf1eb780b
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-03 15:35:52 +0900

    redux

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -8,18 +8,27 @@ const number=document.getElementById("number")
 //data를 수정하기 위한 함수
 // reducer 은 data를 바꾸고 modify 하는걸 책임지는 
 //reducer 나 modifier는 처음으로 data를 바꿔준다.
+//return 하는 것은 나의 appliction 에 있는 data이다.
+//application 의 data를 modify 하고 싶으면 countModifler 를 사용하면 된다.
+//state를 argument(인자)로 가지게 된다
+//유일하게 한 개의 함수만 data를 modify 할 수 있다
+//magicl redux는 data가 기본적으로 한 곳에만 있는 것이다. (countModifler)
+//유일하게 counModifler 만 state를 modify 할 수 있다.
 
-const countModifler =()=>{
-    return "hello";  //이러면 이게 내 data가 될 수 있다.
- 
+
+
+const countModifler =(count = 0)=>{
+    console.log(count)
+    return count;  //이러면 이게 내 data가 될 수 있다.
 }
+//inital state는 state가 undefined 된다.
+
 
 
 
 //create 라는 함수가 우리에게 reducer 를 주기를 요구한다.
 const countStore=createStore(countModifler); //store 생성
 
-console.log(countStore.getState())
 
 
 
